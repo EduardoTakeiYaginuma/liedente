3931c5cde825294ae15e3c24569d560e0cd7d85f
Eduardo Takei Yaginuma <eduyaginuma10@gmail.com>
2026-01-20 16:30:59 -0300

    refactor: remove unused FAQ and Testimonials sections from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import HeroSection from '@/components/home/HeroSection'
 import AboutSection from '@/components/home/AboutSection'
 import CredentialsSection from '@/components/home/CredentialsSection'
-import TestimonialsSection from '@/components/home/TestimonialsSection'
-import FAQSection from '@/components/home/FAQSection'
 
 export default function Home() {
   return (
@@ -10,8 +8,6 @@ export default function Home() {
       <HeroSection />
       <CredentialsSection />
       <AboutSection />
-      <TestimonialsSection />
-      <FAQSection />
     </>
   )
 }
